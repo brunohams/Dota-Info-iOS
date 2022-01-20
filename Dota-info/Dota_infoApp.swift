4bf4bde91bed9de51c06048209c4f277ca0b504f
Bruno Hammes <bruno@premiersoft.net>
2022-01-20 09:54:06 -0300

    Initial Commit

---
+++ Dota-info/Dota_infoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dota_infoApp.swift
+//  Dota-info
+//
+//  Created by PremierSoft on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Dota_infoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dota-infoTests/Dota_infoTests.swift
@@ -0,0 +1,33 @@
+//
+//  Dota_infoTests.swift
+//  Dota-infoTests
+//
+//  Created by PremierSoft on 20/01/22.
+//
+
+import XCTest
+@testable import Dota_info
+
+class Dota_infoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
