5efe256b8ffa344ab00fefe43edf905ba26ed103
Bruno Hammes <bruno@premiersoft.net>
2022-03-07 14:14:24 -0300

    Change RX stream to UseCase Output ports

--- Modules/Hero/Hero_Presentation/Sources/Hero_Presentation/HeroList/HeroListViewModel.swift
+++ Modules/Hero/Hero_Presentation/Sources/Hero_Presentation/HeroList/HeroListViewModel.swift
@@ -10,7 +10,7 @@ class HeroListViewModel: ObservableObject, GetHeroesOutput {
 
     init (getHeroes: GetHeroes) {
         self.getHeroes = getHeroes
-        
+                
         getHeroes.execute(output: self)
     }
 
